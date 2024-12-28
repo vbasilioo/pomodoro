8c1d64b64cb3177ffdbaff531f467eb40a9914a3
vbasilioo <vbasilio2019@gmail.com>
2024-12-28 17:29:41 -0300

    refactor(spatie): remove permission and user-role models

--- app/Filament/Resources/PermissionResource.php
+++ app/Filament/Resources/PermissionResource.php
@@ -13,10 +13,11 @@ use Filament\Tables;
 use Filament\Tables\Table;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Spatie\Permission\Models\Permission as ModelsPermission;
 
 class PermissionResource extends Resource
 {
-    protected static ?string $model = Permission::class;
+    protected static ?string $model = ModelsPermission::class;
 
     protected static ?string $navigationIcon = 'heroicon-o-adjustments-horizontal';
 
