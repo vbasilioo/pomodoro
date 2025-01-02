9bb0bcae7b29b0671254e1f57a8b588d5529eab2
danielhe4rt <danielhe4rt@gmail.com>
2025-01-02 13:06:26 -0300

    feat(deps): add jetstream

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,13 @@ Route::get('/', function () {
 });
 
 Route::get('/switch-language/{locale}', [LangagueSwitchController::class, 'switch'])->name('locale.switch');
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
