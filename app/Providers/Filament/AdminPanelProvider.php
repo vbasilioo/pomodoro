8f068668113c24f3d3640a791c95b022777d2182
vbasilioo <vbasilio2019@gmail.com>
2025-01-05 10:25:18 -0300

    fix(notifications): remove notifications in admin panel

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -52,8 +52,6 @@ class AdminPanelProvider extends PanelProvider
             ])
             ->authMiddleware([
                 Authenticate::class,
-            ])
-            ->databaseNotifications()
-            ->databaseNotificationsPolling('5s');
+            ]);
     }
 }
