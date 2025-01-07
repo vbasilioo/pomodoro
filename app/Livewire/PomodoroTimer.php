58ae063bd58eb93739c94a17599b28e6b0c59ec4
Fro1s <matheuscosta1150@gmail.com>
2025-01-07 17:46:15 -0300

    chore(timer): chore timer display develop

--- app/Livewire/PomodoroTimer.php
+++ app/Livewire/PomodoroTimer.php
@@ -12,60 +12,94 @@ use Livewire\Component;
 
 class PomodoroTimer extends Component
 {
-    protected $listeners = ['completePomodoro'];
+    protected $listeners = ['completePomodoro', 'updateTime'];
 
     public bool $isRunning;
 
-    public Pomodoro $pomodoro;
+    public ?Pomodoro $pomodoro = null;
+
+    public PomodoroTypeEnum $type;
+
+    public int $timeExpected;
 
     public function completePomodoro()
     {
         PomodoroCompleted::dispatch('Pomodoro completed!');
     }
 
+    public function updateTimeRemaining($timeRemaining): void
+    {
+        if ($this->pomodoro && $this->pomodoro->checkStatus(PomodoroStatusEnum::Running)) {
+            $this->pomodoro->update(['time_expected' => $timeRemaining]);
+            $this->dispatch('pomodoroStatus', [
+                'status' => $this->pomodoro->pomodoro_status,
+                'time_remaining' => $timeRemaining,
+            ]);
+        }
+    }
+
     public function render(): View
     {
-        $this->isRunning = false;
-        // TODO: work on guest flow later.
         if (auth()->user()) {
             $pomodoro = auth()->user()->pomodoro()->latest()->first();
 
             if ($pomodoro) {
                 $this->pomodoro = $pomodoro;
-                $this->dispatch('pomodoroStatus', ['status' => $pomodoro->pomodoro_status]);
-            }
 
-            return view('livewire.pomodoro-timer');
+                if ($pomodoro->checkStatus(PomodoroStatusEnum::Running)) {
+                    $elapsedTime = Carbon::now()->diffInSeconds(Carbon::parse($pomodoro->started_at));
+
+                    $this->pomodoro->time_expected = max(0, $pomodoro->time_expected - $elapsedTime);
+
+                    if ($this->pomodoro->time_expected === 0) {
+                        $this->completePomodoro();
+                    }
+                }
+
+                $this->dispatch('pomodoroStatus', [
+                    'status' => $pomodoro->pomodoro_status,
+                    'time_remaining' => $this->pomodoro->time_expected,
+                ]);
+            }
         }
 
-        return view('livewire.pomodoro-timer-guest');
+        return view('livewire.pomodoro-timer', [
+            'timeRemaining' => $this->pomodoro->time_expected ?? 0,
+        ]);
     }
 
     public function startPomodoro(): void
     {
         $pomodoro = auth()->user()->pomodoro()->create([
             'pomodoro_status' => PomodoroStatusEnum::Running,
-            'pomodoro_type' => PomodoroTypeEnum::Regular,
-            'time_expected' => 25 * 60,
+            'pomodoro_type' => $this->type ?? PomodoroTypeEnum::Regular,
+            'time_expected' => $this->timeExpected ?? 1500,
             'started_at' => now(),
         ]);
 
-        $this->dispatch('pomodoroStatus', ['status' => PomodoroStatusEnum::Running->value]);
+        $this->dispatch('pomodoroStatus', [
+            'status' => PomodoroStatusEnum::Running->value,
+            'time_remaining' => $this->timeExpected ?? 1500,
+        ]);
+
         $this->isRunning = true;
         $this->pomodoro = $pomodoro;
     }
 
     public function pausePomodoro(): void
     {
-
-        $remainingTime = $this->pomodoro->time_expected - (int)Carbon::now()->diffInSeconds();
+        $elapsedTime = Carbon::parse($this->pomodoro->started_at)->diffInSeconds();
+        $remainingTime = max(0, $this->pomodoro->time_expected - $elapsedTime);
 
         $this->pomodoro->update([
             'pomodoro_status' => PomodoroStatusEnum::Paused,
-            'time_expected' => $remainingTime
+            'time_expected' => $remainingTime,
         ]);
 
-        $this->dispatch('pomodoroStatus', ['status' => PomodoroStatusEnum::Paused->value]);
+        $this->dispatch('pomodoroStatus', [
+            'status' => PomodoroStatusEnum::Paused->value,
+            'time_remaining' => $remainingTime,
+        ]);
 
         $this->isRunning = false;
     }
@@ -74,6 +108,12 @@ class PomodoroTimer extends Component
     {
         $this->pomodoro->update([
             'pomodoro_status' => PomodoroStatusEnum::Running,
+            'started_at' => now(),
+        ]);
+
+        $this->dispatch('pomodoroStatus', [
+            'status' => PomodoroStatusEnum::Running->value,
+            'time_remaining' => $this->pomodoro->time_expected,
         ]);
 
         $this->isRunning = true;
@@ -81,13 +121,45 @@ class PomodoroTimer extends Component
 
     public function abandonPomodoro(): void
     {
-        $this->pomodoro->update([
-            'pomodoro_status' => PomodoroStatusEnum::Abandoned,
-            'abandoned_at' => now(),
+        if ($this->pomodoro) {
+            $this->pomodoro->update([
+                'pomodoro_status' => PomodoroStatusEnum::Abandoned,
+                'abandoned_at' => now(),
+            ]);
+        }
+
+        $this->dispatch('pomodoroStatus', [
+            'status' => PomodoroStatusEnum::Abandoned->value,
+            'time_remaining' => 0,
         ]);
 
-        $this->dispatch('pomodoroStatus', ['status' => PomodoroStatusEnum::Abandoned->value]);
-
         $this->isRunning = false;
+        $this->pomodoro = null;
+    }
+
+    public function setType($type): void
+    {
+        $this->type = PomodoroTypeEnum::from($type);
+        $this->timeExpected = $this->checkTypeAndTimeExpected($this->type)['time_expected'];
+
+        $this->dispatch('updateTimeRemaining', ['timeRemaining' => $this->timeExpected]);
+    }
+
+    public function checkTypeAndTimeExpected(PomodoroTypeEnum $typeEnum): array
+    {
+        return match ($typeEnum) {
+            PomodoroTypeEnum::Regular => [
+                'time_expected' => 1500,
+                'type' => PomodoroTypeEnum::Regular
+            ],
+            PomodoroTypeEnum::Custom => [
+                'time_expected' => 900,
+                'type' => PomodoroTypeEnum::Custom
+            ],
+            PomodoroTypeEnum::Break => [
+                'time_expected' => 300,
+                'type' => PomodoroTypeEnum::Break
+            ],
+        };
     }
 }
--- resources/views/livewire/pomodoro-timer.blade.php
+++ resources/views/livewire/pomodoro-timer.blade.php
@@ -1,23 +1,30 @@
+@php use App\Enums\PomodoroStatusEnum; @endphp
 <div class="bg-opacity-20 bg-white rounded-lg shadow-lg p-6 w-1/4 text-center">
     <div class="flex justify-center items-center gap-4 mb-6">
-
-        <button id="setFocus" class="px-4 h-8 rounded-md font-bold hover:bg-white focus:bg-white shadow-sm">FOCUS
+        <button wire:click="setType('regular')" id="setFocus" value="regular"
+                class="px-4 h-8 rounded-md font-bold hover:bg-white focus:bg-white focus:outline-none shadow-sm">FOCUS
         </button>
-        <button id="setBreak" class="px-4 h-8 rounded-md font-bold hover:bg-white focus:bg-white shadow-sm">BREAK
+        <button wire:click="setType('break')" id="setBreak" value="break"
+                class="px-4 h-8 rounded-md font-bold hover:bg-white focus:bg-white shadow-sm">BREAK
         </button>
-        <button id="setLongBreak" class="px-4 h-8 rounded-md font-bold hover:bg-white focus:bg-white shadow-sm">LONG
-            BREAK
+        <button wire:click="setType('custom')" id="setLongBreak" value="custom"
+                class="px-4 h-8 rounded-md font-bold hover:bg-white focus:bg-white shadow-sm">LONG BREAK
         </button>
     </div>
 
     <div id="timerDisplay" class="text-7xl font-extrabold mb-6 text-white">
-        123
+        @if(isset($pomodoro))
+            {{ sprintf('%02d:%02d', intdiv($pomodoro->time_expected, 60), $pomodoro->time_expected % 60) }}
+        @else
+            00:00
+        @endif
     </div>
 
     <div class="flex">
-        @if(isset($pomodoro))
-            @if ($pomodoro->checkStatus(\App\Enums\PomodoroStatusEnum::Paused))
-                <button wire:click="resumePomodoro" class="bg-white border-4 border-b-8 justify-start flex border-black px-4 py-2 rounded-md font-bold">
+        @if(isset($pomodoro) && !$pomodoro->checkStatus(PomodoroStatusEnum::Abandoned))
+            @if ($pomodoro->checkStatus(PomodoroStatusEnum::Paused))
+                <button wire:click="resumePomodoro"
+                        class="bg-white border-4 border-b-8 justify-start flex border-black px-4 py-2 rounded-md font-bold">
                     RESUME
                 </button>
             @else
@@ -39,28 +46,30 @@
     </div>
 </div>
 
-
 <script>
-
-    let timeRemaining = {{ $pomodoro?->time_remaining ?? 15 }};
-    let isRunning = {{ $pomodoro?->checkStatus(\App\Enums\PomodoroStatusEnum::Running) ?? false }};
-
+    let timeRemaining = {{ $pomodoro?->time_remaining }};
     let timerInterval = null;
+
+    function updateDisplay() {
+        const minutes = Math.floor(timeRemaining / 60);
+        const seconds = timeRemaining % 60;
+        document.getElementById("timerDisplay").textContent =
+            `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+    }
 
     function startTimer() {
         if (timerInterval) {
             clearInterval(timerInterval);
         }
+
         timerInterval = setInterval(() => {
-            console.log(timeRemaining);
             if (timeRemaining > 0) {
                 timeRemaining--;
                 updateDisplay();
             } else {
-                // Time is up—notify Livewire and stop the interval
-                notifyPomodoroComplete();
                 clearInterval(timerInterval);
                 timerInterval = null;
+                notifyPomodoroComplete();
             }
         }, 1000);
     }
@@ -72,31 +81,31 @@
         }
     }
 
-
-    function updateDisplay() {
-        const minutes = Math.floor(timeRemaining / 60);
-        const seconds = timeRemaining % 60;
-        document.getElementById("timerDisplay").textContent =
-            `${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-    }
-
     function notifyPomodoroComplete() {
         Livewire.dispatch('completePomodoro');
     }
 
-    document.addEventListener('livewire:init', () => {
-        notifyPomodoroComplete();
+    document.addEventListener('livewire:load', () => {
+        updateDisplay();
 
-        Livewire.on('pomodoroStatus', (event) => {
-            console.log(event);
-            if (event[0].status === 'running') {
-                isRunning = true;
+        Livewire.on('pomodoroStatus', (data) => {
+            timeRemaining = data.time_remaining;
+            if (data.status === '{{ PomodoroStatusEnum::Running->value }}') {
                 startTimer();
             } else {
-                isRunning = false;
                 pauseTimer();
             }
         });
 
+        if (timeRemaining > 0) {
+            startTimer();
+        }
+    });
+
+    document.addEventListener('DOMContentLoaded', function () {
+        const focusButton = document.getElementById('setFocus');
+        if (focusButton) {
+            focusButton.focus();
+        }
     });
 </script>
